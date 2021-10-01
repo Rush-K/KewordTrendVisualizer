5781a55a53c75ee4c73ab02fc0c3c2b00cb77cf2
kms <k_rush@naver.com>
2021-10-01 19:25:26 +0900

    Title Animation applied

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,20 @@
 import { Component } from 'react';
 import { useSpring, animated } from 'react-spring';
+import { Typography, Button } from '@mui/material';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+const Title = () => {
+    const styles = useSpring({
+        loop: false,
+        to: [
+          { opacity: 1, color: '#ffaaee' },
+          { opacity: 0, color: 'rgb(14,26,19)' },
+        ],
+        from: { opacity: 0, color: 'red' },
+    });
+
+    return <animated.div style={styles}>I will fade in and out</animated.div>
+}
 
 class Main extends Component {
     constructor(props) {
@@ -9,12 +23,12 @@ class Main extends Component {
 
     render() {
         return (
-            <Parallax pages={2} style={{ top: '0', left: '0' }}>
+            <Parallax pages={3} style={{ top: '0', left: '0' }}>
             <ParallaxLayer
                 offset={0}
                 speed={2.5}
                 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <p>Scroll down</p>
+                <Title />
             </ParallaxLayer>
     
             <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
@@ -28,8 +42,24 @@ class Main extends Component {
                 alignItems: 'center',
                 color: 'white',
                 }}>
-                <p>Scroll up</p>
+                <p>Scroll down</p>
             </ParallaxLayer>
+
+            <ParallaxLayer offset={2} speed={2} style={{ backgroundColor: '#ff6d6c' }} />
+
+            <ParallaxLayer
+                offset={2}
+                speed={2.5}
+                style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                color: 'white',
+                }}>
+                <p>Scroll up</p>
+                <Button>test</Button>
+            </ParallaxLayer>
+
             </Parallax>
         );
     }
