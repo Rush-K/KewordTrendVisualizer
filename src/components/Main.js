80da3a159dbd74c0490cf4a79dbc9b8544f157ab
kms <k_rush@naver.com>
2021-10-01 17:52:05 +0900

    Initial Setting

---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import { useSpring, animated } from 'react-spring';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Parallax pages={2} style={{ top: '0', left: '0' }}>
+            <ParallaxLayer
+                offset={0}
+                speed={2.5}
+                style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <p>Scroll down</p>
+            </ParallaxLayer>
+    
+            <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
+    
+            <ParallaxLayer
+                offset={1}
+                speed={0.5}
+                style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                color: 'white',
+                }}>
+                <p>Scroll up</p>
+            </ParallaxLayer>
+            </Parallax>
+        );
+    }
+}
+
+export default Main;
