b48ea89ded65f6f97b4f06ba48fbb9e4debe5875
kms <k_rush@naver.com>
2021-10-26 23:21:12 +0900

    Menu UI Design

---
+++ src/components/Menu.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import { Box, Paper, Divider, 
+         List, ListItem, ListItemIcon, ListItemText, ListItemButton,
+         styled, createTheme, ThemeProvider } from '@mui/material';
+
+const media = [
+    { label: '더쿠' },
+    { label: '네이트판'},
+    { label: '루리웹'}
+];
+
+const FireNav = styled(List)({
+    '& .MuiListItemButton-root': {
+      paddingLeft: 24,
+      paddingRight: 24,
+    },
+    '& .MuiListItemIcon-root': {
+      minWidth: 0,
+      marginRight: 16,
+    },
+    '& .MuiSvgIcon-root': {
+      fontSize: 20,
+    },
+  });
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Box sx={{ display: "flex" }}>
+                <ThemeProvider
+                    theme={createTheme({
+                        components: {
+                            MuiListItemButton: {
+                                defaultProps: {
+                                disableTouchRipple: true,
+                                },
+                            },
+                        },
+                        palette: {
+                            mode: 'dark',
+                            primary: { main: 'rgb(102, 157, 246)' },
+                            background: { paper: 'rgb(5, 30, 52)' },
+                        },
+                        typography: {
+                            fontFamily: 'baemin',
+                        }
+                })}>
+                    <Paper elevation={0} sx={{ width: 256, height: "100vh" }}>
+                        <FireNav component="nav" disablePadding>
+                            <ListItemButton component="a" href="/visualizer">
+                                <ListItemIcon sx={{ fontSize: 20 }}>🔥</ListItemIcon>
+                                <ListItemText
+                                    sx={{ my: 0 }}
+                                    primary="Keyword"
+                                    primaryTypographyProps={{
+                                    fontSize: 20,
+                                    fontWeight: 'medium',
+                                    letterSpacing: 0,}}/>
+                            </ListItemButton>
+                            <Divider />
+                            <ListItem sx={{bgcolor: "rgba(71, 98, 130, 0.2)"}}>
+                            <ListItemText
+                                primary="커뮤니티 목록"
+                                primaryTypographyProps={{
+                                    fontSize: 20,
+                                    fontWeight: 'medium',
+                                    lineHeight: '30px',
+                                    mb: '2px',
+                                }}
+                                secondary="다양한 키워드를 알아보세요!"
+                                secondaryTypographyProps={{
+                                    noWrap: true,
+                                    fontSize: 12,
+                                    lineHeight: '16px',
+                                    color: 'rgba(255,255,255,0.5)',
+                                }}
+                                sx={{ my: 0 }}/>
+                            </ListItem>
+                            <Divider />
+                            {media.map((item) => (
+                                <ListItemButton
+                                key={item.label}
+                                sx={{ py: 0, minHeight: 32, color: 'rgba(255,255,255,.8)' }}>
+                                    <ListItemText
+                                    primary={item.label}
+                                    primaryTypographyProps={{ fontSize: 15, fontWeight: 'medium' }}
+                                    />
+                                </ListItemButton>
+                            ))}
+                            <Divider />
+                        </FireNav>
+                    </Paper>
+                </ThemeProvider>
+            </Box>
+        );
+    }
+}
+
+export default Menu;
--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import { Typography, Box, AppBar, Toolbar, Button, IconButton, 
-         Drawer, List, ListItem, ListItemIcon, ListItemText, Divider } from '@mui/material';
-import { Menu, Inbox, Mail } from '@mui/icons-material';
 import { withStyles } from '@mui/styles';
+import Menu from './Menu';
 
 const useStyles = (theme) => ({
     background: {
         background: "rgb(240,240,240)",
-        width: "100%",
+        width: "100vw",
         height: "100vh"
     }
 });
@@ -21,50 +19,7 @@ class Visualizer extends Component {
         const { classes } = this.props;
         return (
             <div id="background" className={classes.background}>
-                <div id="appbar">
-                    <Box sx={{ display: "flex" }}>
-                        <AppBar style={{background: "rgb(50,50,50)"}} position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-                            <Toolbar>
-                            <IconButton
-                                size="large"
-                                edge="start"
-                                color="inherit"
-                                aria-label="menu"
-                                sx={{ mr: 2 }}
-                            >
-                                <Menu />
-                            </IconButton>
-                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} style={{fontFamily: "baemin"}}>
-                                Keyword Trend Visualizer
-                            </Typography>
-                            <Button color="inherit">Past Page</Button>
-                            </Toolbar>
-                        </AppBar>
-                        <Drawer
-                            variant="permanent"
-                            sx={{
-                            width: "240",
-                            flexShrink: 0,
-                            [`& .MuiDrawer-paper`]: { width: "240", boxSizing: 'border-box', background: "rgb(100,100,100)" },
-                            }}
-                        >
-                            <Toolbar />
-                            <Box sx={{ overflow: 'auto' }}>
-                            <List>
-                                {['1', '2', '3', '4'].map((text, index) => (
-                                <ListItem button key={text}>
-                                    <ListItemIcon>
-                                    {index % 2 === 0 ? <Inbox /> : <Mail />}
-                                    </ListItemIcon>
-                                    <ListItemText primary={text} style={{color: "rgb(255,255,255)"}} />
-                                </ListItem>
-                                ))}
-                            </List>
-                            <Divider />
-                            </Box>
-                        </Drawer>
-                    </Box>
-                </div>
+                <Menu />
             </div>
 
         );
