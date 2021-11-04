efb10e26a2d969a879141662876604c8b6b196d1
kms <k_rush@naver.com>
2021-11-05 00:29:55 +0900

    neologism Top 10 chart

---
+++ src/components/Charts/TopFivePolarAreaChart.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Chart from 'react-apexcharts';
+
+class TopFivePolarAreaChart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            series: this.props.oovwordData.map((d) => { return parseInt(d.value) }),
+            options: {
+                chart: {
+                    type: 'polarArea',
+                  },
+                  stroke: {
+                    colors: ['#fff']
+                  },
+                  fill: {
+                    opacity: 0.8
+                  },
+                  responsive: [{
+                    breakpoint: 480,
+                    options: {
+                      chart: {
+                        width: 200
+                      },
+                      legend: {
+                        position: 'bottom'
+                      }
+                    }
+                  }],
+                  labels: this.props.oovwordData.map((d) => { return d.label }),
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Chart options={this.state.options} series={this.state.series} type="polarArea" height="90%"/>
+        );
+    }
+}
+
+export default TopFivePolarAreaChart;
